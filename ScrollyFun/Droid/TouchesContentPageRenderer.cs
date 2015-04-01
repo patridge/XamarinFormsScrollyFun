c6afa72660d57e9ec4804793634fbd7403702a83
Adam Patridge <patridge@gmail.com>
2015-03-31 22:26:29 -0600

    [And] Custom renderer implementation with touch events.

---
+++ ScrollyFun/Droid/TouchesContentPageRenderer.cs
@@ -0,0 +1,48 @@
+﻿using System.Drawing;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using Android.Views;
+using System.Linq;
+using View = Android.Views.View;
+
+[assembly: ExportRenderer (typeof(ScrollyFun.TouchesContentPage), typeof(ScrollyFun.Droid.TouchesContentPageRenderer))]
+namespace ScrollyFun.Droid
+{
+	public class TouchesContentPageRenderer : VisualElementRenderer<TouchesContentPage>
+	{
+		public override bool OnTouchEvent (MotionEvent e)
+		{
+			var touches = Enumerable.Range (0, e.PointerCount).Select (i => {
+				var pointerId = e.GetPointerId(i);
+				var location = new PointF {
+					X = e.GetX(i),
+					Y = e.GetY(i),
+				};
+				return new IdentifiedTouch {
+					Id = pointerId,
+					Location = location,
+				};
+			}).ToArray();
+
+			MotionEventActions action = e.Action & MotionEventActions.Mask;
+			switch (action) {
+			case MotionEventActions.Down:
+			case MotionEventActions.PointerDown:
+				Element.TouchesBegan (touches);
+				break;
+			case MotionEventActions.Move:
+				Element.TouchesMoved (touches);
+				break;
+			case MotionEventActions.Up:
+			case MotionEventActions.PointerUp:
+				Element.TouchesEnded (touches);
+				break;
+			case MotionEventActions.Cancel:
+				Element.TouchesCancelled (touches);
+				break;
+			}
+
+			return true;
+		}
+	}
+}
