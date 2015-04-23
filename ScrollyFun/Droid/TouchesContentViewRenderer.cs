a9650b23fb40c3243c6b1006b97d358459f09108
Adam Patridge <patridge@gmail.com>
2015-04-23 09:06:38 -0600

    [Android] {fix} Resolve pixel/point differences.

--- ScrollyFun/Droid/TouchesContentViewRenderer.cs
+++ ScrollyFun/Droid/TouchesContentViewRenderer.cs
@@ -4,20 +4,26 @@ using Xamarin.Forms.Platform.Android;
 using Android.Views;
 using System.Linq;
 using View = Android.Views.View;
+using System.Collections.Generic;
 
 [assembly: ExportRenderer(typeof(ScrollyFun.TouchesContentView), typeof(ScrollyFun.Droid.TouchesContentViewRenderer))]
 namespace ScrollyFun.Droid {
     public class TouchesContentViewRenderer : VisualElementRenderer<TouchesContentView> {
+        int ConvertToDp(double pixels) {
+            return (int)(pixels / Resources.DisplayMetrics.Density);
+        }
+        PointF ConvertToDp(PointF pixelsPoint) {
+            return new PointF(ConvertToDp(pixelsPoint.X), ConvertToDp(pixelsPoint.Y));
+        }
         public override bool OnTouchEvent(MotionEvent e) {
             var touches = Enumerable.Range(0, e.PointerCount).Select(i => {
                 var pointerId = e.GetPointerId(i);
-                var location = new PointF {
-                    X = e.GetX(i),
-                    Y = e.GetY(i),
-                };
+                // Android X/Y are based on full pixel count, not the density-independent pixels X.F works in.
+                var androidLocation = new PointF(e.GetX(i), e.GetY(i));
+                var formsLocation = ConvertToDp(androidLocation);
                 return new IdentifiedTouch {
                     Id = pointerId,
-                    Location = location,
+                    Location = formsLocation,
                 };
             }).ToArray();
 
